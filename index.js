7ad783b49c45ae9082870e8ea0ca33b82174f9a7
Colin Kwok <colinkwok@Colins-MacBook-Pro.local>
2019-10-05 15:16:24 -0700

    init push

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path');
+const PORT = process.env.PORT || 5000
+var app = express();
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('index'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
