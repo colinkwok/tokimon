087988d1315e86f05c87240e91a2c9cb0ceddd2e
Colin Kwok <colinkwok@Colins-MacBook-Pro.local>
2019-10-05 23:15:44 -0700

    postgres tutorial

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const path = require('path');
+const db = require('./queries')
 const PORT = process.env.PORT || 5000
 var app = express();
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('index'))
-
-.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect()
-    const result = await client.query('SELECT * FROM test_table');
-    const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
+app.get('/test', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
 })
+app.get('/users', db.getUsers)
+
+
 
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
