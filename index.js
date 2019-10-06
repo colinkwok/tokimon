10c2aa010f50d15bf877d7c33ca05003e8215585
Colin Kwok <colinkwok@Colins-MacBook-Pro.local>
2019-10-05 17:20:07 -0700

    .env, procfile added. set up psql

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('index'))
+
+.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
