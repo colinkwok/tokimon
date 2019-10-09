270661989b0bbcd98be6c38c61a092cd286336c6
Colin Kwok <colinkwok@Colins-MacBook-Pro.local>
2019-10-08 23:00:33 -0700

    add and delete feature

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express')
-// const bodyParser = require('body-parser')
 const path = require('path');
-// const db = require('./queries')
 const PORT = process.env.PORT || 5000
 var app = express();
 
@@ -9,12 +7,7 @@ const { Pool } = require('pg');
 var pool;
 pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
-  // user: 'postgres',
-  // host: 'localhost',
-  // database: 'users',
-  // password: 'root',
-  // port: 5432,
+  ssl: true // comment out if developing locally
 });
 
 
@@ -30,41 +23,65 @@ app.get('/tokimon',(req,res) => {
   var getUsersQuery = `SELECT * FROM Tokimon`;
   pool.query(getUsersQuery, (error,result) => {
     if (error)
-      res.end(error);
+      res.end(error.toString());
     var results = {'rows': result.rows};
     res.render('./tokimon',results)  
   })
 })
-
-// app.get('/test', (request, response) => {
-//   pool.query('SELECT * FROM users_table;', (error, results) => {
-//     if (error) {
-//       throw error
-//     }
-//     response.status(200).json(results.rows)
-//   })
-// })
-
-// app.get('/search', (req,res) => {
-//   pool.query('SELECT * FROM users_table;', (err, result) => {
-//     if (err) {
-//       return console.error('Error executing query', err.stack)
-//     }
-//     res.json(result.rows)
-//   })
-// })
-// app.get('/tokimon', (req,res) => {
-//   res.render('tokimon');
-// })
-
-app.post('/login', (req,res) => {
+app.get('/add', (req, res) => {
+  res.render('add')
+})
+app.post('/added', (req,res) => {
   // console.log('post');
   // console.log(req.body);
   // console.log(req.body);
-  var username = req.body.user;
-  var password = req.body.pwd;
-  res.send(`Hello, ${username}.  You have password ${password}`);
+  var name = req.body.name;
+  var weight = req.body.weight;
+  var height = req.body.height;
+  var fly = req.body.fly;
+  var fight = req.body.fight;
+  var fire = req.body.fire;
+  var water = req.body.water;
+  var electric = req.body.electric;
+  var frozen = req.body.frozen;
+  var total = parseFloat(fly) + parseFloat(fight) + parseFloat(fire) + parseFloat(water) + parseFloat(electric) + parseFloat(frozen);
+  var trainer = req.body.trainer;
+  var addTokiQuery = `INSERT INTO Tokimon (name, weight, height, fly, fight, fire, water, electric, frozen, total, trainer) VALUES ('${name}',${weight},${height},${fly},${fight},${fire},${water},${electric},${frozen},${total},'${trainer}')`
+  console.log(addTokiQuery);
+  pool.query(addTokiQuery, (error,result) => {
+    if (error)
+      res.end(error) 
+    res.send(`Tokimon: '${name}',${weight},${height},${fly},${fight},${fire},${water},${electric},${frozen},${total},'${trainer}' has been added to the database`);
+  })
 });
+app.get('/delete', (req, res) => {
+  var getUsersQuery = `SELECT * FROM Tokimon`;
+  pool.query(getUsersQuery, (error,result) => {
+    if (error)
+      res.end(error.toString());
+    var results = {'rows': result.rows};
+    res.render('./delete',results)  
+  })
+})
+app.post('/deleted', (req,res) => {
+  // console.log(req.body);
+  var tokimonid = req.body.tokimonid;
+  var deleteQuery = `DELETE FROM Tokimon WHERE tokimonid = '${tokimonid}'`;
+  pool.query(deleteQuery, (error,result) => {
+    if (error)
+      res.end(error.toString());
+    res.send(`Tokimon With ID: ${tokimonid} has been deleted from the database`);
+  })
+});
+
+// app.post('/login', (req,res) => {
+//   // console.log('post');
+//   // console.log(req.body);
+//   // console.log(req.body);
+//   var username = req.body.user;
+//   var password = req.body.pwd;
+//   res.send(`Hello, ${username}.  You have password ${password}`);
+// });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
---
+++ public/js/script.js
@@ -0,0 +1,26 @@
+
+document.getElementById("deleteBTN").addEventListener('click', function() {
+    console.log("Hello"); 
+});
+
+document.addEventListener('keyup', function() {
+    console.log("Yo");
+});
+
+
+
+
+
+var deleteTokimon = document.getElementById("deleteConfirm");
+deleteTokimon.onclick = function() {     
+    console.log("Hello");                   
+    var table = document.getElementById("tokimon_table");   
+    for (var i = 1; i < table.rows.length; i++) {           
+        var checkbox = rows[i].cells[0];                    
+       if (checkBox.checked == true){                      
+          console.log(parseInt(rows[i].cells[1].innerHTML)); 
+        }                                                   
+    }                                                    
+}                                                          
+
+
